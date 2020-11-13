36930a7318190a696319fe4c66a33fe096d0a02a
i837801 <chris.de.castro@sap.com>
2020-11-12 22:23:08 -0600

    Adding unit test

---
+++ src/test/java/com/cdc/api1/CandidateApiIT.java
@@ -0,0 +1,66 @@
+package com.cdc.api1;
+
+import com.cdc.api1.entity.Candidate;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class CandidateApiIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getsAllCandidates() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/candidate")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    public void getsSingleCandidate() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/candidate/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    public void returnsNotFoundSingleCandidate() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/candidate/99")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+    @Test
+    public void addsNewCandidate() throws Exception {
+
+        Candidate candidate = new Candidate(3L, "michael", "jordan", "michael@jordan.com", 3);
+        ObjectMapper objectMapper = new ObjectMapper();
+        mockMvc.perform(MockMvcRequestBuilders.post("/candidate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(candidate))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+}
